6259d61eed2d80445a3b7a0b42c679d468ee13bd
xybayue-tbk <xybayue@gmail.com>
2026-06-11 16:02:30 +0700

    menambahkan void inorder dan isinya

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -60,4 +60,17 @@ public:
             }
         }
     }
+    
+    void inorder(Node* ptr) {
+        if(root == NULL) {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if(ptr != NULL) {
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+        }
+    }
+
 };
