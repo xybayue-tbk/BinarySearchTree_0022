7c4eabfa7caf0c6b2caf80690610a980bfe1697a
xybayue-tbk <xybayue@gmail.com>
2026-06-11 16:08:07 +0700

    menambahkan perulangan

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -101,4 +101,18 @@ public:
 
 int main() {
     BinaryTree obj;
+
+    while(true) {
+        cout << "\nMenu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Perform inorder traversal" << endl;
+        cout << "3. Perform preorder traversal" << endl;
+        cout << "4. Perform postorder traversal" << endl;
+        cout << "5. Exit" << endl;
+        cout << "\nEnter your choice: ";
+
+        char ch;
+        cin >> ch;
+
+        cout << endl;
 }
