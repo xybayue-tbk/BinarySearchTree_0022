8cd90bac7e7b185722ad68969dd3f26c9c951946
xybayue-tbk <xybayue@gmail.com>
2026-06-11 15:56:45 +0700

    menambahkan modifaier public dan constuctor Node

--- SearchTree.cpp
+++ SearchTree.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 class Node {
+public:
     string info;
     Node* leftchild;
     Node* rightchild;
+
+    Node(string i, Node* l, Node* r) {
+        info = i;
+        leftchild = l;
+        rightchild = r;
+    }
 };
